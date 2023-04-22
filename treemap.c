de9f24561a8075efd2e310191eda0542c71adacd
Replit user <>
2023-04-22 23:17:20 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -47,7 +47,54 @@ TreeMap *createTreeMap(int (*lower_than)(void *key1, void *key2)) {
   return new;
 }
 
-void insertTreeMap(TreeMap *tree, void *key, void *value) {}
+void insertTreeMap(TreeMap *tree, void *key, void *value) {
+  TreeNode *new;
+
+  if (tree->root == NULL) {
+    new = createTreeNode(key, value);
+    tree->root = new;
+    return;
+  }
+
+  tree->current = tree->root;
+
+  while (tree->current != NULL) {
+    if (is_equal(tree, key, tree->current->pair->key) == 1) {
+      return;
+    }
+    if (tree->lower_than(key, tree->current->pair->key) == 1) {
+      if (tree->current->left == NULL) {
+        new = createTreeNode(key, value);
+        tree->current->left = new;
+        new->parent = tree->current;
+        tree->current = tree->current->left;
+        return;
+      }
+      tree->current = tree->current->left;
+    } else {
+      if (tree->current->right == NULL) {
+        new = createTreeNode(key, value);
+        tree->current->right = new;
+        new->parent = tree->current;
+        tree->current = tree->current->right;
+        return;
+      }
+      tree->current = tree->current->right;
+    }
+  }
+}
+
+TreeNode *minimum(TreeNode *x) {
+  TreeNode *current = x;
+
+  while (current != NULL) {
+    if (current->left == NULL)
+      return current;
+    else
+      current = current->left;
+  }
+  return NULL;
+}
 
 TreeNode *minimum(TreeNode *x) { return NULL; }
 
