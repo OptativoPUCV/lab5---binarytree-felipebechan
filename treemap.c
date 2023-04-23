70f78a2a24f50cbeeaae9f79abe1aa41cdd65bc4
Replit user <>
2023-04-23 02:44:02 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -202,15 +202,6 @@ Pair *firstTreeMap(TreeMap *tree) {
 
 Pair *nextTreeMap(TreeMap *tree) {
   TreeNode *aux = tree->current;
-  if (aux->right != NULL) {
-    aux = minimum(aux->right);
-  } else {
-    TreeNode *parent = aux->parent;
-    while (parent != NULL && aux == parent->right) {
-      aux = parent;
-      parent = parent->parent;
-    }
-    aux = parent;
-  }
+  return NULL;
 }
 // mamma mia
